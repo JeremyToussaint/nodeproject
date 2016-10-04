14777193d200b7eec97a737da56b844b1bc3fc45
Fabien Virot <fabien.virot@cpe.fr>
2016-10-04 11:34:37 +0200

    app config modifies

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+var express =  require("express");
+
+var app = express();
+var http = require("http");
+var CONFIG = require("./config.json");
+
+process.env.CONFIG = JSON.stringify(CONFIG);
+
+var server = http.createServer(app);
+server.listen(CONFIG.port);
+
 console.log("It works !")
